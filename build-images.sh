d308d1b5eb6b4db5633395f319a3efb42b43fbfc
Wallacy <wallacyrezende@gmail.com>
2023-05-16 00:49:20 -0300

    [MAIN] - refactor: docker files and fix bugs

--- build-images.sh
+++ build-images.sh
@@ -2,15 +2,19 @@
 
 cd service-discovery
 sh docker-image.sh
+cd ..
 
 cd config-server
 sh docker-image.sh
+cd ..
 
 cd api-gateway
 sh docker-image.sh
+cd ..
 
 cd auth-server
 sh docker-image.sh
+cd ..
 
 cd ms-user-release
 sh docker-image.sh
