3e0cf10e329b790e7b1db4796ce580628a132cc6
Wallacy Rezende <wallacyrezende@gmail.com>
2024-06-01 21:07:28 -0300

    feat: adjust configs

--- auth-server/src/test/java/com/dev/finance/authserver/AuthServerApplicationTests.java
+++ auth-server/src/test/java/com/dev/finance/authserver/AuthServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.dev.finance.authserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServerApplicationTests {
 
     @Test
